01850e5033354e0055ed7a34098113f0cdf93873
Paulo Civardi <paulocivardi@gmail.com>
2023-11-27 20:31:10 -0400

    findAll and insert on class DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program2 {
 
@@ -17,8 +21,18 @@ public class Program2 {
 		System.out.println("=== TEST 1: department findById ====");
 		Department department = departmentDao.findById(1);
 		System.out.println(department);
-
 		
+		System.out.println("\n=== TEST 2: department findAll ====");
+		List<Department> list = new ArrayList<>();
+		list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== TEST 3: department insert ====");
+		Department newdepartment = new Department(null, "FB");
+		departmentDao.insert(newdepartment);
+		System.out.println("Inserted new id = " + newdepartment.getId());
 		
 	}
 
